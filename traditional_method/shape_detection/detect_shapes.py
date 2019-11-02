ccfb70abbbcbb9296c78cb4dca6d297f3579cb1d
kai <177358582@qq.com>
2019-11-02 23:57:44 +0800

    analyse rotation in traditional method

--- traditional_method/shape_detection/detect_shapes.py
+++ traditional_method/shape_detection/detect_shapes.py
@@ -12,25 +12,43 @@ from traditional_method.shape_detection.pyimagesearch.shapedetector import Shape
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
 	help="path to the input image")
+ap.add_argument("-d", "--debug", required=False, default=True,
+	help="whether to set the debug mode(draw some pictures)")
 args = vars(ap.parse_args())
 
 # load the image and resize it to a smaller factor so that
 # the shapes can be approximated better
 image = cv2.imread(args["image"])
+debug = args['debug']
 resized = imutils.resize(image, width=300)
 ratio = image.shape[0] / float(resized.shape[0])
 
 # convert the resized image to grayscale, blur it slightly,
 # and threshold it
 gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+if debug == True:
+	cv2.imshow('after_thresh',gray)
+	cv2.waitKey(0)
 blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+if debug == True:
+	cv2.imshow('after_thresh',blurred)
+	cv2.waitKey(0)
+
 thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
+if debug == True:
+	cv2.imshow('after_thresh',thresh)
+	cv2.waitKey(0)
 
 # find contours in the thresholded image and initialize the
 # shape detector
 cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
 	cv2.CHAIN_APPROX_SIMPLE)
+
 cnts = imutils.grab_contours(cnts)
+if debug == True:
+	cv2.drawContours(resized,cnts,-1,(255,0,0), thickness=8)
+	cv2.imshow('draw_contour',resized)
+	cv2.waitKey(0)
 sd = ShapeDetector()
 
 # loop over the contours
@@ -40,7 +58,7 @@ for c in cnts:
 	M = cv2.moments(c)
 	cX = int((M["m10"] / M["m00"]) * ratio)
 	cY = int((M["m01"] / M["m00"]) * ratio)
-	shape = sd.detect(c)
+	shape,corner_points,angle = sd.detect(c)
 
 	# multiply the contour (x, y)-coordinates by the resize ratio,
 	# then draw the contours and the name of the shape on the image
@@ -48,7 +66,7 @@ for c in cnts:
 	c *= ratio
 	c = c.astype("int")
 	cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
-	cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
+	cv2.putText(image, shape+' '+str(angle)+'Degree', (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
 		0.5, (255, 255, 255), 2)
 
 	# show the output image
--- traditional_method/shape_detection/pyimagesearch/shapedetector.py
+++ traditional_method/shape_detection/pyimagesearch/shapedetector.py
@@ -1,5 +1,27 @@
 # import the necessary packages
 import cv2
+import math
+import operator
+def cal_angle(v1,v2):
+	angle1 = math.atan2(v1[1], v1[0])
+	angle1 = int(angle1 * 180 / math.pi)
+	angle2 = math.atan2(v2[1], v2[0])
+	angle2 = int(angle2 * 180 / math.pi)
+	if angle1 * angle2 >= 0:
+		included_angle = abs(angle1 - angle2)
+	else:
+		included_angle = abs(angle1) + abs(angle2)
+		if included_angle > 180:
+			included_angle = 360 - included_angle
+	return included_angle
+
+
+def get_calibration(shape_name):
+	if shape_name == 'triangle':
+		length_order=[0,2,1]
+		return length_order
+
+
 
 class ShapeDetector:
 	def __init__(self):
@@ -8,12 +30,43 @@ class ShapeDetector:
 	def detect(self, c):
 		# initialize the shape name and approximate the contour
 		shape = "unidentified"
+		angle = 0
 		peri = cv2.arcLength(c, True)
 		approx = cv2.approxPolyDP(c, 0.04 * peri, True)
+		corner_points = [[point[0][0],point[0][1]] for point in approx]
+		mass_point = [0,0]
+		for point in corner_points:
+			mass_point[0] += point[0]
+			mass_point[1] += point[1]
+		mass_point[0] /= len(corner_points)
+		mass_point[1] /= len(corner_points)
+		cenral_corner_points =[[point[0]-mass_point[0],point[1]-mass_point[1]] for point in corner_points]
+		vectors = [[(corner_points[i][0]-corner_points[i-1][0]),(corner_points[i][1]-corner_points[i-1][1])] for i in range(len(corner_points))]
+		angles = []
+		for i in range(len(vectors)):
+			angles.append(int(math.atan2(vectors[i][1],vectors[i][0])*180/math.pi))
 
+		angles_between = []
+		for i in range(len(vectors)-1):
+			angles_between.append(cal_angle(vectors[i],vectors[i+1]))
+		angles_between.append(cal_angle(vectors[-1], vectors[0]))
+		# order the the corner points according to the length of each vector
+		lengths = []
+		for i in range(len(vectors)):
+			lengths.append([pow(vectors[i][0],2)+pow(vectors[i][1],2),i])
+		lengths.sort(key=lambda x:x[0])
+		length_order = [pair[1] for pair in lengths]
 		# if the shape is a triangle, it will have 3 vertices
 		if len(approx) == 3:
 			shape = "triangle"
+			cali_order = get_calibration(shape)
+			first_cali_element = cali_order[0]
+			first_actual_element = length_order[0]
+			move = abs(first_actual_element-first_cali_element)%3
+			angle =math.atan2(vectors[move][1], vectors[move][0])
+			angle = int(angle * 180 / math.pi)
+
+
 
 		# if the shape has 4 vertices, it is either a square or
 		# a rectangle
@@ -26,14 +79,26 @@ class ShapeDetector:
 			# a square will have an aspect ratio that is approximately
 			# equal to one, otherwise, the shape is a rectangle
 			shape = "square" if ar >= 0.95 and ar <= 1.05 else "rectangle"
+			if shape == 'square':
+				absAngles = map(abs, angles)
+				absAngles = list(absAngles)
+				angle = angles[absAngles.index(min(absAngles))]
+			else:
+				absAngles = map(abs, angles)
+				absAngles = list(absAngles)
+				angle = angles[absAngles.index(min(absAngles))]
 
 		# if the shape is a pentagon, it will have 5 vertices
 		elif len(approx) == 5:
 			shape = "pentagon"
+			absAngles = map(abs,angles)
+			absAngles = list(absAngles)
+			angle = angles[absAngles.index(min(absAngles))]
 
 		# otherwise, we assume the shape is a circle
 		else:
 			shape = "circle"
+			angle = None
 
 		# return the name of the shape
-		return shape
+		return shape, approx, angle
