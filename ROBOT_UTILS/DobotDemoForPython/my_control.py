0f81a71669b00b1d13fd642cad4eafa9764c9d05
rkterence <rkterence@zju.edu.cn>
2019-11-29 14:18:58 +0800

    utilities tested and bugs fixed

--- ROBOT_UTILS/DobotDemoForPython/my_control.py
+++ ROBOT_UTILS/DobotDemoForPython/my_control.py
@@ -9,24 +9,27 @@ if __name__ == "__main__":
     dobot.initialize()
     dobot.set_home()  # self-examine to restore precision
     print("Finished setting home. Preparing to execute working session commands...")
+    print("Position: ", dobot.get_pos()[0], '\n')
 
     while True:  # "True" can be replaced by a condition: whether or not to continue
         # Robot and Vision module reset:
         dobot.move(dobot.w_pos, is_immediate=True)  # wait in the waiting position
+        print("Position: ", dobot.get_pos()[0])
 
         # obj_pos = vision.detect()
-        obj_pos = np.array([200, 50, 50, 50])
+        obj_pos = np.array([200, 100, 50, 50])
 
         dobot.move(obj_pos)
         # dobot.move(obj_pos-[0, 0, 30, 0])
-        dobot.end_control(on=True)  # grab the object
+        # dobot.end_control(on=True)  # grab the object
         dobot.execute_cmd_then_stop()
         # dType.dSleep(5000)
 
         dobot.move(dobot.des_pos)
-        dobot.wait(1000)
-        dobot.end_control(on=False)
+        dobot.wait(1)
+        # dobot.end_control(on=False)
         dobot.execute_cmd_then_stop()
+        print("Position: ", dobot.get_pos()[0], '\n')
 
         """vision: check whether there is remaining blocks"""
         # vision.detect_remaining()
