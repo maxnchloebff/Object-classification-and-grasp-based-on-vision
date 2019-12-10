d14d66b878ec39afef1defdaa813c2c94d5f505f
rkterence <rkterence@zju.edu.cn>
2019-12-10 18:52:12 +0800

    Tiny improvements for calibration

--- ROBOT_UTILS/DobotDemoForPython/test_io_multiplexing.py
+++ ROBOT_UTILS/DobotDemoForPython/test_io_multiplexing.py
@@ -5,6 +5,8 @@ pin = 16
 
 dobot = DobotMagician()
 dobot.initialize()
+dobot.set_home()
+dobot.start_execute_cmd()
 dobot.set_mode_digital_output(pin=pin, is_immediate=True)
 print("Initialized")
 
@@ -13,8 +15,9 @@ dobot.move(dobot.w_pos, is_immediate=True)
 for i in range(5):
     dobot.set_digital_output(pin=pin, level=False, is_immediate=True)
     print("Now the output is LOW")
-    time.sleep(20)
+    time.sleep(5)
     dobot.set_digital_output(pin=pin, level=True, is_immediate=True)
     print("Now the output is HIGH")
+    time.sleep(5)
 
 dobot.disconnect()
